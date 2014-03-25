e4108d462103583acdbdb6463cd9753bf0690870
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-03-25 11:27:19 -0600

    It lives! code can now run an empty window

--- src/highfive/charactersheet/Main.java
+++ src/highfive/charactersheet/Main.java
@@ -1,7 +1,10 @@
 package highfive.charactersheet;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame mainWindow = new MainWindow("Character Sheet");
+        mainWindow.setVisible(true);
     }
 }
--- src/highfive/charactersheet/MainWindow.java
+++ src/highfive/charactersheet/MainWindow.java
@@ -1,6 +1,12 @@
 package highfive.charactersheet;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private CharacterSheetView characterSheetView;
+
+    public MainWindow(String title) throws HeadlessException {
+        super(title);
+    }
 }
