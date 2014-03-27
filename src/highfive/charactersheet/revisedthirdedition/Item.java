b4513f69ba5490f351fb3249ff4833d385de9cb7
Marcus Hahne <mehahne@gmail.com>
2014-03-27 10:35:43 -0600

    added Inventory, Item classes

---
+++ src/highfive/charactersheet/revisedthirdedition/Item.java
@@ -0,0 +1,35 @@
+package highfive.charactersheet.revisedthirdedition;
+
+/**
+ * Created by Marcus on 3/27/2014.
+ */
+public class Item {
+    private String name;
+    private double weight;
+
+    public Item(){
+        this.name = "Undefined";
+        this.weight = 0.0;
+    }
+
+    public Item(String name, double weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
