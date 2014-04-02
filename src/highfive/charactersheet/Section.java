c27b2d27bdd8cf84a6b9b98e56477cb0e6dc80cb
Matt <mggreen85@hotmail.com>
2014-04-02 08:20:11 -0600

    Testing

--- src/highfive/charactersheet/Section.java
+++ src/highfive/charactersheet/Section.java
@@ -1,30 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package highfive.charactersheet;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import javax.swing.*;
+import javax.swing.JPanel;
+import java.awt.*;
 
-public class Section extends JPanel {
-    private String title;
-    private Icon hide;
-    private Icon unhide;
 
-    /**
-     * If the section is expanded, vertically collapses the section.
-     * This method was referred to in specification as hide(),
-     * but hide() is an existing method of JPanel and should not
-     * be overwritten.
-     */
-    public void collapse() {
-        throw new NotImplementedException();
+/**
+ *
+ * @author Matt
+ */
+public class Section {
+
+    private JButton collapseButton;
+    private JPanel jPanel1;
+    private JPanel collapsePanel;
+    private JTextField jTextField1;
+    private JPanel MainPanel;
+    private JFrame myFrame; //for testing
+    boolean unhide;
+
+    public Section()
+    {
+        unhide=true;
+        initComponents();
+    }
+
+    private void initComponents() {
+        MainPanel = new JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        collapseButton = new javax.swing.JButton();
+        collapsePanel = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        myFrame = new JFrame();
+        myFrame.show();
+        //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(collapseButton))
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(collapseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        jTextField1.setText("1");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout collapsePanelLayout = new javax.swing.GroupLayout(collapsePanel);
+        collapsePanel.setLayout(collapsePanelLayout);
+        collapsePanelLayout.setHorizontalGroup(
+                collapsePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(collapsePanelLayout.createSequentialGroup()
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 304, Short.MAX_VALUE))
+        );
+        collapsePanelLayout.setVerticalGroup(
+                collapsePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(collapsePanelLayout.createSequentialGroup()
+                                .addGap(34, 34, 34)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(199, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout MainPanelLayout = new javax.swing.GroupLayout(MainPanel);
+        MainPanel.setLayout(MainPanelLayout);
+        MainPanelLayout.setHorizontalGroup(
+                MainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(collapsePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        MainPanelLayout.setVerticalGroup(
+                MainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(MainPanelLayout.createSequentialGroup()
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(collapsePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        GroupLayout layout = new GroupLayout(myFrame.getContentPane());
+        myFrame.getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(MainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(MainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        collapseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                collapseButtonActionPerformed(evt);
+            }
+        });
+
+    }
+    /*{
+        collapsePanel = new JPanel();
+        //jLabel1 = new JLabel();
+        jPanel1 = new JPanel();
+        collapseButton = new JButton();
+        jTextField1 = new javax.swing.JTextField();
+
+        //incase they want to add text to the Text Box!
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+
+        });
+
+        //Add a text box to Collapseable Panel
+        collapsePanel.add(jTextField1);
+
+        
+
+        jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(collapseButton);
+    }
+    */
+
+    //When they hit the collapse button it will minimize the code.
+    private void collapseButtonActionPerformed(java.awt.event.ActionEvent evt) {
+        unhide = !unhide;
+        collapsePanel.setVisible(unhide);
     }
 
     /**
-     * If the section is collapsed, vertically expands the section.
-     * This method was referred to in specification as unhide().
-     * See the javadoc for hide()
+     * @param args the command line arguments
      */
-    public void expand() {
-        throw new NotImplementedException();
+    public static void main(String[] args) {
+        Section a = new Section();
+    }
+
+    //
+    /**
+     * Allows the user to edit the text
+     */
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
+        //Empty
     }
 }
