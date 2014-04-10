179e27d924435f488eb86f57f92e61d7224d7de6
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-04-10 14:29:02 -0600

    correct a typo

--- src/highfive/charactersheet/revisedthirdedition/models/Alignment.java
+++ src/highfive/charactersheet/revisedthirdedition/models/Alignment.java
@@ -2,6 +2,6 @@ package highfive.charactersheet.revisedthirdedition.models;
 
 public enum Alignment {
     CHAOTIC_EVIL, CHAOTIC_NEUTRAL, CHAOTIC_GOOD,
-    NEUTRAL_EVIL, NETRUAL,         NEUTRAL_GOOD,
+    NEUTRAL_EVIL, NEUTRAL,         NEUTRAL_GOOD,
     LAWFUL_EVIL,  LAWFUL_NEUTRAL,  LAWFUL_GOOD
 }
