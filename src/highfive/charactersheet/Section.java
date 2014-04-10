704c82655a45668a54c994a0214c83ad0d612d8c
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-04-10 14:23:39 -0600

    remove deprecated nested panel

--- src/highfive/charactersheet/Section.java
+++ src/highfive/charactersheet/Section.java
@@ -2,27 +2,11 @@ package highfive.charactersheet;
 
 import org.jdesktop.swingx.JXTaskPane;
 
-import javax.swing.*;
-import java.awt.*;
-
 public abstract class Section extends JXTaskPane {
 
-    private JPanel contentArea;
 
     public Section(String title) {
         setTitle(title);
-        contentArea = new JPanel();
-        add(contentArea);
-    }
-
-    @Deprecated
-    public Component addContent(Component comp) {
-        return contentArea.add(comp);
-    }
-
-    @Deprecated
-    public void setContentLayout(LayoutManager mgr) {
-        contentArea.setLayout(mgr);
     }
 
     /**
